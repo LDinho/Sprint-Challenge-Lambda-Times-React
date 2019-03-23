7a70669e6276f3e3070c473a80ba97c651e7a65b
LD <LDinho@users.noreply.github.com>
2019-03-23 12:59:54 -0400

    add Tabs to render on page

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import Tab from './Tab';
+
 const Tabs = props => {
+  const { tabs, selectTabHandler, selectedTab } = props;
+
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {tabs.map((tab, index) => {
+          // console.log(tab);
+          return (
+            <Tab key={index}
+                 tab={tab}
+                 selectTabHandler={selectTabHandler}
+                 selectedTab={selectedTab}
+            />
+          )
+
+        })}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+
 export default Tabs;
