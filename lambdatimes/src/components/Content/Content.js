e6c3ad2c946246548859af40dc2deb714e959dff
LD <LDinho@users.noreply.github.com>
2019-03-23 13:35:17 -0400

    add setState() to changeSelected method -- selected tab style updates on click of tabs

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -26,7 +26,10 @@ export default class Content extends Component {
   }
 
   changeSelected = tab => {
-    // this function should take in the tab and update the state with the new tab.
+
+    this.setState({
+      selected: tab,
+    })
   };
 
   filterCards = () => {
@@ -48,11 +51,6 @@ export default class Content extends Component {
   render() {
     return (
       <div className="content-container">
-        {/*
-          Add 2 props to the Tabs component,
-          `selectedTab` that includes the currently selected tab
-          and `selectTabHandler` that includes the function to change the selected tab
-        */}
         <Tabs tabs={this.state.tabs}
               selectedTab={this.state.selected}
               selectTabHandler={this.changeSelected}
