e22b8b685e0a64cb0fc6bb775c629d4fcd0d9760
LD <LDinho@users.noreply.github.com>
2019-03-23 15:06:10 -0400

    [Stretch] WIP- implement carousel

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
-// Complete this Carousel 
+
 export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      carousel: [],
+      index: 0,
     }
   }
-  componentDidMount(){
 
+  componentDidMount(){
+    this.setState({
+      carousel: carouselData,
+    })
   }
 
   leftClick = () => {
-
+    this.setState({
+      index: this.state.index - 1,
+    })
   }
 
   rightClick = () => {
-
+    this.setState({
+      index: this.state.index + 1,
+    })
   }
 
   selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    return <img src={''} style={{display: 'block'}} />
   }
-  
+
   render(){
+    // const {carousel} = this.state;
+
     return (
       <div className="carousel">
+        <img src="./assets/carousel/mountains.jpeg" style={{display: 'block'}}/>
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
